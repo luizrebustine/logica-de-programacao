f9a65286d09f99dc8511e8e2afbbbfd724d7e3c7
Luiz Rebustine <122850565+luizrebustine@users.noreply.github.com>
2024-05-21 16:52:36 -0300

    finalização do exercício

--- sistema-de-votacao/Votacao2024.java
+++ sistema-de-votacao/Votacao2024.java
@@ -5,6 +5,7 @@ public class Votacao2024 {
         Votacao[] votacao = new Votacao[200];
         VotacaoMetodos v = new VotacaoMetodos();
         int opc = 0;
+        int opc2 = 0;
         while(opc != 9){
             opc = Integer.parseInt(JOptionPane.showInputDialog("-------------------Sistema de Votação--------------------\n1 - Carregar Seção"
                     + "/Número Eleitor\n2 - Classificar por Seção\n3 - Gravar Registros\n4 - Mostrar Indicadores\n9 - Finalizar"));
@@ -13,10 +14,31 @@ public class Votacao2024 {
                     v.FCadastraVotacao(votacao);
                     break;
                 case 2:
+                    v.FClassificaSecao(votacao);
                     break;
                 case 3:
                     v.FGravaVotacao(votacao);
                     break;
+                case 4:
+                    while(opc2 != 9){
+                        opc2 = Integer.parseInt(JOptionPane.showInputDialog("-------------------Sistema de Votação--------------------\n1 - Quantidade Eleitores por Seção"
+                                + "\n2 - Seção com Maior e Menor número de Eleitores\n3 - Quantidade de votos por candidato\n4 - 10 primeiros colocadas (nro cand. e qtd votos)\n9 - Finaliza Consulta"));
+                        switch(opc2){
+                            case 1:
+                                v.FQuantidadeCandidatos(votacao);
+                                break;
+                            case 2:
+                                v.FSeçãoMaiorMenor(votacao);
+                                break;
+                            case 3:
+                                v.FVotosPorCandidato(votacao);
+                                break;
+                            case 4:
+                                v.FDezCandidatos(votacao);
+                                break;
+                        }
+                    }
+                break;
             }
         }
     }
--- sistema-de-votacao/VotacaoMetodos.java
+++ sistema-de-votacao/VotacaoMetodos.java
@@ -7,15 +7,32 @@ public class VotacaoMetodos {
             votacao[i] = new Votacao();
         }
         for(i=0;i<200;i++){
-            votacao[i].NumeroSecao = (int)(Math.random()*10);
+            votacao[i].NumeroSecao = (int)(Math.random()*11);
             votacao[i].NumeroCandidato = (int)(Math.random()*300);
         }
         return votacao;
-        //Usar bubble sort para ordenar os registros.
     }
     public void FClassificaSecao(Votacao[] votacao){
         int i, j, aux;
-        
+        for(i=0;i<200;i++){
+            for(j=0;j<199;j++){
+                if(votacao[j].NumeroSecao > votacao[j + 1].NumeroSecao){
+                    aux = votacao[j].NumeroSecao;
+                    votacao[j].NumeroSecao = votacao[j+1].NumeroSecao;
+                    votacao[j+1].NumeroSecao = aux;
+                    aux = votacao[j].NumeroCandidato;
+                    votacao[j].NumeroCandidato = votacao[j+1].NumeroCandidato;
+                    votacao[j+1].NumeroCandidato = aux;
+                }
+            }
+        }
+        /*for(i=0;i<200;i++){
+            System.out.print(votacao[i].NumeroSecao + " ");
+        }
+        System.out.print("\n");
+        for(i=0;i<200;i++){
+            System.out.print(votacao[i].NumeroCandidato + " ");
+        }*/
     }
     public void FGravaVotacao(Votacao[] votacao)throws IOException{
         String nomearquivo = "votacao.txt";
@@ -29,5 +46,87 @@ public class VotacaoMetodos {
         }
         System.out.println("GRAVAÇÃO FEITA COM SUCESSO");
         gravar.close();
+    }
+    public void FQuantidadeCandidatos(Votacao[] votacao){
+        int i;
+        int[] vetorquantidade = FCalculaSeção(votacao);
+        for(i=0;i<=10;i++){
+            System.out.println("Quantidade de candidatos do número " + i + " = " + vetorquantidade[i]);
+        }
+    }
+    public int[] FCalculaSeção(Votacao[] votacao){
+        int i, j;
+        int quantidade = 0;
+        int[] vetorquantidade = new int[11];
+        for(i=0;i<=10;i++){
+            for(j=0;j<200;j++){
+                if(votacao[j].NumeroSecao == i){
+                    quantidade ++;
+                }
+            }
+        vetorquantidade[i] = quantidade;
+        quantidade = 0;
+        }
+        return vetorquantidade;
+    }
+    public void FSeçãoMaiorMenor(Votacao[] votacao){
+        int i, j, aux;
+        int[] vetorquantidade = FCalculaSeção(votacao);
+        for(i=0;i<=10;i++){
+            for(j=0;j<10;j++){
+                if(vetorquantidade[j+1] > vetorquantidade[j]){
+                    aux = vetorquantidade[j];
+                    vetorquantidade[j] = vetorquantidade[j+1];
+                    vetorquantidade[j+1] = aux;
+                }
+            }
+        }
+        System.out.print("Maior Seção: " + vetorquantidade[0] + " Menor Seção: " + vetorquantidade[10]);
+    }
+    public int[] FCalculaCandidato(Votacao[] votacao){
+        int i, j;
+        int quantidade = 0;
+        int[] vetorquantidade = new int[300];
+        for(i=0;i<300;i++){
+            for(j=0;j<200;j++){
+                if(votacao[j].NumeroCandidato == i){
+                    quantidade ++;
+                }
+            }
+        vetorquantidade[i] = quantidade;
+        quantidade = 0;
+        }
+        return vetorquantidade;
+    }
+    public void FVotosPorCandidato(Votacao[] votacao){
+        int i;
+        int[] vetorquantidade = FCalculaCandidato(votacao);
+        for(i=0;i<300;i++){
+            System.out.println("Quantidade de votos do candidato " + i + " = " + vetorquantidade[i]);
+        }
+    }
+    public void FDezCandidatos(Votacao[] votacao){
+        int i, j, aux;
+        String zeroesquerda = "%s";
+        int[] vetorquantidade = FCalculaCandidato(votacao);
+        int[] vetorindice = new int[300];
+        for(i=0;i<300;i++){
+            vetorindice[i] = i;
+        }
+        for(i=0;i<300;i++){
+            for(j=0;j<299;j++){
+                if(vetorquantidade[j+1] > vetorquantidade[j]){
+                    aux = vetorquantidade[j];
+                    vetorquantidade[j] = vetorquantidade[j+1];
+                    vetorquantidade[j+1] = aux;
+                    aux = vetorindice[j];
+                    vetorindice[j] = vetorindice[j+1];
+                    vetorindice[j+1] = aux;
+                }
+            }
+        }
+        for(i=0;i<10;i++){
+            System.out.println(String.format("%02d", (i+1)) + "° - candidato " + String.format("%03d", vetorindice[i]) + ", " + vetorquantidade[i] + " votos");
+        }
     }
 }
